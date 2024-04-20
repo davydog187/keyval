0adfa78389b3a57ed2e50616cd0b28ac9d6664ac
Dave Lucia <davelucianyc@gmail.com>
2024-04-20 09:55:11 -0400

    implement the first part

--- main.go
+++ main.go
@@ -2,18 +2,107 @@ package main
 
 import (
 	"bufio"
+	"errors"
 	"fmt"
 	"os"
+	"strings"
 )
 
-func Add(a int, b int) int {
-	return a + b
+type State struct {
+	Data map[string]string
+}
+
+type Command interface {
+	ExecuteCommand(State) (string, error)
+}
+
+type Read struct {
+	key string
+}
+
+type Write struct {
+	key string
+	val string
+}
+
+type Delete struct {
+	key string
+}
+
+func (c Read) ExecuteCommand(state State) (string, error) {
+	if val, ok := state.Data[c.key]; ok != false {
+		return val, nil
+	}
+
+	return fmt.Sprintf("Key not found: %s", c.key), nil
+}
+
+func (c Write) ExecuteCommand(state State) (string, error) {
+	state.Data[c.key] = c.val
+
+	return "", nil
+}
+
+func (c Delete) ExecuteCommand(state State) (string, error) {
+	delete(state.Data, c.key)
+
+	return "", nil
+}
+
+func ParseCommand(line string) (Command, error) {
+	tokens := strings.Split(line, " ")
+
+	switch tokens[0] {
+	case "READ":
+		if len(tokens) != 2 {
+			return nil, errors.New("READ must have a key")
+		}
+
+		key := tokens[1]
+		return Read{key}, nil
+	case "WRITE":
+		if len(tokens) != 3 {
+			return nil, errors.New("WRITE must have a key and value")
+		}
+		key := tokens[1]
+		val := tokens[2]
+		return Write{key, val}, nil
+	case "DELETE":
+		if len(tokens) != 2 {
+			return nil, errors.New("DELETE must have a key")
+		}
+
+		key := tokens[1]
+		return Delete{key}, nil
+
+	default:
+		return nil, errors.New("Failed to parse command")
+	}
 }
 
 func main() {
+	state := State{Data: map[string]string{}}
+
 	scanner := bufio.NewScanner(os.Stdin)
 	for scanner.Scan() {
-		fmt.Println("got: " + scanner.Text())
+		line := scanner.Text()
+		command, err := ParseCommand(line)
+
+		if err != nil {
+			fmt.Errorf("Bad command %s because %s", line, err)
+			continue
+		}
+
+		message, err := command.ExecuteCommand(state)
+
+		if err != nil {
+			fmt.Errorf("Unable to execute command %v because %s", command, err)
+			continue
+		}
+
+		if message != "" {
+			fmt.Println(message)
+		}
 	}
 	if err := scanner.Err(); err != nil {
 		fmt.Fprintln(os.Stderr, "reading standard input:", err)
--- main_test.go
+++ main_test.go
@@ -4,23 +4,51 @@ import (
 	"testing"
 )
 
-func TestAdd(t *testing.T) {
+func TestParseCommand(t *testing.T) {
 	var tests = []struct {
-		name   string
-		a      int
-		b      int
-		result int
+		line    string
+		command Command
 	}{
-		{"1 plus 1", 1, 1, 2},
-		{"2 plus 2", 2, 2, 5},
+		{line: "READ a", command: Read{key: "a"}},
+		{line: "WRITE a b", command: Write{key: "a", val: "b"}},
+		{line: "DELETE a", command: Delete{key: "a"}},
 	}
 
 	for _, test := range tests {
-		t.Run(test.name, func(t *testing.T) {
-			if result := Add(test.a, test.b); result != test.result {
-				t.Errorf("expected %d + %d to be %d got %d", test.a, test.b, test.result, result)
+		t.Run(test.line, func(t *testing.T) {
+			if result, err := ParseCommand(test.line); result != test.command {
+				if err != nil {
+					t.Errorf("received error %v", err)
+				}
+
+				t.Errorf("expected %+v to be %+v got %+v", test.line, test.command, result)
 			}
 		})
 	}
 
+}
+
+func TestExecuteReadCommand(t *testing.T) {
+	var tests = []struct {
+		state   State
+		command Command
+		message string
+	}{
+		{state: State{Data: map[string]string{}}, command: Read{key: "a"}, message: "Key not found: a"},
+		{state: State{Data: map[string]string{"a": "1"}}, command: Read{key: "a"}, message: "1"},
+	}
+
+	for _, test := range tests {
+		t.Run(test.message, func(t *testing.T) {
+			message, err := test.command.ExecuteCommand(test.state)
+
+			if err != nil {
+				t.Errorf("expected no error, got %s", err)
+			}
+
+			if message != test.message {
+				t.Errorf("expected %s but got %s", test.message, message)
+			}
+		})
+	}
 }
